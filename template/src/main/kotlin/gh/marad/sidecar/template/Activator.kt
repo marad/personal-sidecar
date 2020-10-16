1065b8d6e8fcfb5d132265f0b96c62ece73af501
Marcin Radoszewski <marcin.radoszewski@allegro.pl>
2020-10-16 18:33:55 +0200

    Add maven plugin

--- template/src/main/kotlin/gh/marad/sidecar/template/Activator.kt
+++ template/src/main/kotlin/gh/marad/sidecar/template/Activator.kt
@@ -1,14 +1,15 @@
-package gh.marad.sidecar.hello
+package gh.marad.sidecar.template
 
 import org.osgi.framework.BundleActivator
 import org.osgi.framework.BundleContext
 
 class Activator : BundleActivator {
     override fun start(context: BundleContext?) {
-        println("Bundle installed!")
+        println("Bundle started!!")
     }
 
     override fun stop(context: BundleContext?) {
+        println("Bundle stopped!")
     }
 
 }
