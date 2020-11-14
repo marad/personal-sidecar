75b1a999f37f20e4fa45cb10024644c108e543a2
Marcin Radoszewski <moriturius@gmail.com>
2020-11-14 09:10:27 +0100

    Clear unused methods from Inbox.kt

--- obsidian-vault/src/main/kotlin/gh/marad/sidecar/obsidian/vault/Inbox.kt
+++ obsidian-vault/src/main/kotlin/gh/marad/sidecar/obsidian/vault/Inbox.kt
@@ -15,45 +15,13 @@ class Inbox internal constructor(private val inbox: Path) {
         makeInboxIfNotExists()
     }
 
-    private val tagName = "#task"
-    private val clearMessage = "Wyczyścić inbox"
-
     fun updateContent(updateFunc: InboxModifier.() -> Unit) {
         val flatnote = FlatnoteConfig().createFlatnote()
-        val inboxContent = InboxModifier(flatnote.parse(readLines()))
+        val inboxContent = InboxModifier(flatnote.parse(Files.readAllLines(inbox)))
         updateFunc.invoke(inboxContent)
         val markdown = flatnote.render(inboxContent.blocks())
         Files.write(inbox, markdown.toByteArray())
     }
-
-    fun markForClearing() {
-        val contents = readLines()
-        if(!contents.hasLeadingTaskTag()) {
-            contents.apply { add(0, "$tagName $clearMessage") }.save()
-        }
-    }
-
-    fun removeClearTask() {
-        val contents = readLines()
-        if(contents.hasLeadingTaskTag()) {
-            contents.removeAt(0)
-            contents.save()
-        }
-    }
-
-    fun appendNote(note: String) {
-        appendItem(note)
-    }
-
-    fun appendUrl(url: String, comment: String?) {
-        appendItem(if (comment != null) "$url - $comment" else url)
-    }
-
-    private fun String.hasLeadingTaskTag() = this.startsWith(tagName)
-    private fun List<String>.hasLeadingTaskTag() = this.firstOrNull()?.hasLeadingTaskTag() ?: false
-    private fun appendItem(item: String) { readLines().apply { add("- $item") }.save() }
-    private fun readLines(): MutableList<String> = Files.readAllLines(inbox)
-    private fun List<String>.save() = Files.write(inbox, this)
 
     private fun ensureInboxIsMarkdownFile() {
         if(!inbox.toString().endsWith(".md")) {
